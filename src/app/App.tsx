02444024c67a5449accd25058be7116abff0d586
raavmx <raa@videomatrix.ru>
2024-06-03 11:37:32 +0500

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          <p>
+            О себе: Меня зовут Андрей. Около 5 лет фуллстек на ASP.netcore +TelerikUI +(MSSQL, PostgreSQL), до этого,
+            более 10 лет на WinForms + MSSQL.
+          </p>
+          <p>Цель: В компании возникла необходимость перенести весь фронт с ASP.netcore и Telerik.UI на реакт.</p>
+          <p>
+            До этого дня, опыта в реакт не было, поэтому мне нужно все, что требуется для создания современного
+            приложения.
+          </p>
         </p>
       </header>
     </div>
