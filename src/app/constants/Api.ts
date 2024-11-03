adad0a68801f979e027bb32cddeaf6a9d85b9868
raavmx <raa@videomatrix.ru>
2024-11-03 21:14:30 +0500

    fix

--- src/app/constants/Api.ts
+++
@@ -1,4 +0,0 @@
-export const SERVER_URL = 'https://19429ba06ff2.vps.myjino.ru/api';
-export const SERVER_GQL = 'https://cea3c11a3f62.vps.myjino.ru/graphql';
-export const UPLOAD_SERVER_URL = 'http://cea3c11a3f62.vps.myjino.ru/api/upload';
-export const COMMAND_ID = 'raavmx';
